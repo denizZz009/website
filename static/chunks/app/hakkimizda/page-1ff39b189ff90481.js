f82adb4d8027a6a77835e2c91fd9e6daa6585a74
Deniz Egemen Emare <degemenemr0146@gmail.com>
2025-04-19 19:10:58 +0200

    Add files via upload

---
+++ static/chunks/app/hakkimizda/page-1ff39b189ff90481.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[384,766],{213:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,7970,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[970,441,587,358],()=>s(213)),_N_E=e.O()}]);
